b592d871af65c0550ce78e8d055c86067d6cbe61
kingsaqr <abwr67932@gmail.com>
2025-07-24 02:15:43 +0300

    Add files via upload

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "King Saqr Bot",
   botOwner: 'ابو ذياب', // Your name
-  ownerNumber: '966541873053', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '994405571044', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
